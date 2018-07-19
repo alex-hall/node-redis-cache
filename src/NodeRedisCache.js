ce78906de0d77f7759cf1d6504d5bb771af354b8
Alex Hall <me@jalexhall.com>
2018-07-19 16:38:18 -0400

    Build initial set method!!
    TODO: 
    
    	- Build Integration tests to make sure mock cache API is accurate
    	- Extract out promisify logic?
    	- Expand upon redis config, allow stuff like expiry to be set

--- src/NodeRedisCache.js
+++ src/NodeRedisCache.js
@@ -1,9 +1,20 @@
-class NodeRedisCache {
+const redis = require("redis")
+const {promisify} = require("util")
 
-    constructor(){
+module.exports = class NodeRedisCache {
 
+    constructor(clientConfig) {
+        this.client = new redis.createClient(clientConfig)
+
+        this.defaultExpiry = clientConfig.defaultExpiry
     }
+
+    set(key, value, expiry) {
+        const asyncSet = promisify(this.client.set).bind(this.client)
+
+        return asyncSet(key, value, "EX", expiry)
+    }
+
 
 }
 
-module.exports = NodeRedisCache
